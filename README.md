bb1ce47c855ad45381653def17db367fe63eebcf
CristianX <thecristianx@hotmail.com>
2020-05-16 15:45:49 -0500

    Combinando Streams con RxDart

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 A new Flutter project.
 
+## Dependencias
+- Rx Dart `rxdart: ^0.24.1`
+
 ## Getting Started
 
 This project is a starting point for a Flutter application.
--- lib/src/bloc/login_bloc.dart
+++ lib/src/bloc/login_bloc.dart
@@ -5,21 +5,27 @@ import 'dart:async';
 // Validators
 import 'package:formvalidation/src/bloc/validators.dart';
 
+// RX Dart
+import 'package:rxdart/rxdart.dart';
+
 class LoginBloc  with Validators {
 
-
-  final _emailController = StreamController<String>.broadcast();
-  final _passwordController = StreamController<String>.broadcast();
+  // Cambiando StreamController por BehaviorSubject para usar la extensión rx (Por defecto hace un broadcast)
+  final _emailController    = BehaviorSubject<String>();
+  final _passwordController = BehaviorSubject<String>();
 
   // Recuperar los datos del Stream
   Stream<String> get emailStream => _emailController.stream.transform( validarEmail );
   Stream<String> get passwordStream => _passwordController.stream.transform( validarPassword );
 
+  // Stream para validar si ambos tienen data (Combinando streams)
+  Stream<bool> get formValidStream =>  CombineLatestStream.combine2(emailStream, passwordStream, (e, p) => true);
+
   // Getters y Setters
 
   // Insertar valores al stream
-  Function (String) get changeEmail => _emailController.sink.add;
-  Function (String) get changePassword => _passwordController.sink.add;
+  Function(String) get changeEmail => _emailController.sink.add;
+  Function(String) get changePassword => _passwordController.sink.add;
 
 
   // Cerrando Streams
